a89edac47a1a4ea1cb7737ee4c39c7d02105e566
iakev <kirimikmwongera@gmail.com>
2023-06-28 02:33:16 -0700

    Modified to remove unhandled promise exception

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -12,25 +12,29 @@ export default class UsersController {
     }
     if (!userEmail) {
       response.status(400).json({error :'Missing email'});
+      response.end();
     }
     if (!userPassword) {
       response.status(400).json({error: 'Missing password'});
+      response.end();
     }
     const db = dbClient.db;
     const users = db.collection('users');
     const query = {'email': userEmail};
-    try {
-      const user = await users.findOne(query); 
-      if (!user) {
-        const passwordHash = crypto.createHash('sha1').update(userPassword).digest('hex');
-        const user = {email: userEmail, password: passwordHash};
-        const result = await users.insertOne(user);
-        response.status(201).json({'id': result.insertedId, email: userEmail});
-      } else {
-        response.status(400).json({error: 'Already exist'});
+    if ( userEmail && userPassword) {
+      try {
+        const user = await users.findOne(query); 
+        if (!user) {
+          const passwordHash = crypto.createHash('sha1').update(userPassword).digest('hex');
+          const user = {email: userEmail, password: passwordHash};
+          const result = await users.insertOne(user);
+          response.status(201).json({'id': result.insertedId, email: userEmail});
+        } else {
+          response.status(400).json({error: 'Already exist'});
+        }
+      } catch (error) {
+        console.error(error);
       }
-    } catch (error) {
-      console.error(error);
     }
   }
 }
